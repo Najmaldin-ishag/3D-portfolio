2931c3aa2da8787ea41e278a5b4bd29f7ead1c94
Najmaldin-ishag <115111332+Najmaldin-ishag@users.noreply.github.com>
2025-04-17 23:11:25 +0200

    added ShowCaseSection.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./Components/sections/Hero";
+import ShowCaseSection from "./Components/sections/ShowCaseSection";
 
 function App() {
   return (
     <>
       <Hero />
+      <ShowCaseSection />
     </>
   );
 }
