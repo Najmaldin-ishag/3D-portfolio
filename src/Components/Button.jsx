73f4c9964dd6215984e92b5e272706962d5a263c
Najmaldin-ishag <115111332+Najmaldin-ishag@users.noreply.github.com>
2025-04-17 22:27:06 +0200

    added event handler to the Button.jsx

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,6 +1,22 @@
 function Button({ text, id, className }) {
   return (
-    <a className={`${className ?? ""} cta-wrapper`}>
+    <a
+      onClick={(e) => {
+        e.preventDefault();
+        const target = document.getElementById(id);
+        if (target && id) {
+          const offset = window.innerHeight * 0.15;
+
+          const top =
+            target.getBoundingClientRect().top + window.scrollY - offset;
+          window.scrollTo({
+            top: top,
+            behavior: "smooth",
+          });
+        }
+      }}
+      className={`${className ?? ""} cta-wrapper`}
+    >
       <div className="cta-button group">
         <div className="bg-circle" />
         <p className="text">{text}</p>
