ef56eef567e1f2a49a14000c23ad777f12e37054
Najmaldin-ishag <115111332+Najmaldin-ishag@users.noreply.github.com>
2025-04-17 22:41:57 +0200

    updated

--- src/Components/AnimatedCounter.jsx
+++ src/Components/AnimatedCounter.jsx
@@ -1,3 +1,4 @@
+import CountUp from "react-countup";
 import { counterItems } from "../../constants";
 
 function AnimatedCounter() {
@@ -5,12 +6,17 @@ function AnimatedCounter() {
     <div id="counter" className="padding-x-lg xl:mt-0 mt-32">
       <div className="mx-auto grid-4-cols">
         {counterItems.map((item) => (
-          <div
-            key={item.label}
-            className="counter-number text-white text-5xl font-bold mb-2"
-          >
-            0 {item.suffix}
-          </div>
+          <>
+            <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
+              <div
+                key={item.label}
+                className="counter-number text-white text-5xl font-bold mb-2"
+              >
+                <CountUp suffix={item.suffix} end={item.value} />
+              </div>
+              <div>{item.label}</div>
+            </div>
+          </>
         ))}
       </div>
     </div>
