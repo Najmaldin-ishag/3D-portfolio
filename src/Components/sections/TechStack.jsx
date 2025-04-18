24c38ab3a68accc67b9be7b466a492331ce5b93d
Najmaldin-ishag <115111332+Najmaldin-ishag@users.noreply.github.com>
2025-04-18 23:51:53 +0200

    updated TechStack.jsx

--- src/Components/sections/TechStack.jsx
+++ src/Components/sections/TechStack.jsx
@@ -1,8 +1,37 @@
-import { techStackIcons } from "../../../constants";
+import { techStackIcons, techStackImgs } from "../../../constants";
 import TechIcon from "../Models/TechLogos/TechIcon";
 import TitleHeader from "../TitleHeader";
+// import { gsap } from "@gsap/react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const TechStack = () => {
+  useGSAP(() => {
+    // This animation is triggered when the user scrolls to the #skills wrapper
+    // The animation starts when the top of the wrapper is at the center of the screen
+    // The animation is staggered, meaning each card will animate in sequence
+    // The animation ease is set to "power2.inOut", which is a slow-in fast-out ease
+    gsap.fromTo(
+      ".tech-card",
+      {
+        // Initial values
+        y: 50, // Move the cards down by 50px
+        opacity: 0, // Set the opacity to 0
+      },
+      {
+        // Final values
+        y: 0, // Move the cards back to the top
+        opacity: 1, // Set the opacity to 1
+        duration: 1, // Duration of the animation
+        ease: "power2.inOut", // Ease of the animation
+        stagger: 0.2, // Stagger the animation by 0.2 seconds
+        scrollTrigger: {
+          trigger: "#skills", // Trigger the animation when the user scrolls to the #skills wrapper
+          start: "top center", // Start the animation when the top of the wrapper is at the center of the screen
+        },
+      }
+    );
+  });
   return (
     <div id="skills" className="flex-center section-padding">
       <div className="w-full h-full md:px-10 px-5">
@@ -29,6 +58,18 @@ const TechStack = () => {
               </div>
             </div>
           ))}
+
+          {/* {techStackImgs.map((icon) => (
+            <div
+              key={icon.name}
+              className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
+            >
+              <div className="tech-card-animated-bg" />
+              <div className="tech-card-content">
+                <div className=""></div>
+              </div>
+            </div>
+          ))} */}
         </div>
       </div>
     </div>
